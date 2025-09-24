cea0066f5a1a72d77298aefe5ab97628fa4e0f0e
Physick <96335032+DegustatorPonos@users.noreply.github.com>
2025-09-24 21:38:12 +0500

    Update telescope.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,2 +1,3 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = 'Telescope find files' })
