aaeb4d0a0f9cf8a81d37660773950c5f0252d994
Physick <96335032+DegustatorPonos@users.noreply.github.com>
2025-10-26 01:22:42 +0500

    Backgrout transparency toggler

--- nvim/lua/physick/remap.lua
+++ nvim/lua/physick/remap.lua
@@ -1,5 +1,28 @@
+IsTransparent = false
+function ToggleVisibility() 
+    if not IsTransparent then 
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        IsTransparent = true
+    else 
+        vim.cmd('colorscheme '..vim.g.colors_name)
+        IsTransparent = false
+    end
+end
+
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("x", "p", "P")
 vim.keymap.set("x", "P", "p")
 vim.keymap.set("v", "v", "<C-v>")
+
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz',  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz',  { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<C-j>', ':cnext<CR>',  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ':cprev<CR>',  { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>t', ':Themery<CR>',  { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-t>t", ToggleVisibility)
+vim.keymap.set("n", "<C-t><C-t>", ToggleVisibility)
