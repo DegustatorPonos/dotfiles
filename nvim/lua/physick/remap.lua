ac3114ec8fb5e5436584ebd4a2f98befb40b7413
Physick <96335032+DegustatorPonos@users.noreply.github.com>
2026-04-26 18:52:55 +0500

    Oil nvim

--- nvim/lua/physick/remap.lua
+++ nvim/lua/physick/remap.lua
@@ -11,7 +11,8 @@ function ToggleVisibility()
 end
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.api.nvim_set_keymap('n', '<leader>e', ':Oil<CR>',  { noremap = true, silent = true })
+-- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("x", "p", "P")
 vim.keymap.set("x", "P", "p")
 vim.keymap.set("v", "v", "<C-v>")
