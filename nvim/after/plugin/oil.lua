d4572b46a6c1f93bf3815cc3f76fb42b6263709a
physcik <mynameisgennadiy@vk.com>
2026-04-29 22:45:09 +0500

    'Sex!' fix

--- nvim/after/plugin/oil.lua
+++ nvim/after/plugin/oil.lua
@@ -1,1 +1,17 @@
-require("oil").setup()
+vim.g.loaded_netrw = 1
+
+require("oil").setup({
+    default_file_explorer = true,
+    use_default_keymaps = true,
+})
+
+vim.api.nvim_create_user_command("Explore", "Oil <args>", { nargs = "?", complete = "dir" })
+vim.api.nvim_create_user_command("E", "Explore <args>", { nargs = "?", complete = "dir" })
+
+vim.api.nvim_create_user_command("Sexplore", function(opts)
+    local cmd = opts.bang and "topleft vsplit" or "topleft split"
+    vim.cmd(cmd)
+    require("oil").open(opts.args ~= "" and opts.args or nil)
+end, { nargs = "?", complete = "dir", bang = true })
+vim.api.nvim_create_user_command("Vexplore", "rightbelow vsplit | Oil <args>", { nargs = "?", complete = "dir" })
+vim.api.nvim_create_user_command("Texplore", "tabedit % | Oil <args>", { nargs = "?", complete = "dir" })
--- nvim/init.lua
+++ nvim/init.lua
@@ -10,3 +10,4 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 require("physick")
 require("config.lazy")
+
